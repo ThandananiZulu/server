f58aa88814275e7c51df0369c9b10a11cedcdbe2
ThandananiZulu <91917069+ThandananiZulu@users.noreply.github.com>
2023-01-24 02:27:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ io.on("connection", (socket) => {
     io.to(users[event]).emit("message", message, sender);
   });
 });
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 server.listen(port, function hostname() {
   console.log(`listening on: ${port}`);
 });
